0fe83c5426405c65b04b43338108f46657350c49
falun <github@magichappyplace.com>
2019-02-09 12:11:54 -0800

    adding a spec

---
+++ spec/test_spec.js
@@ -0,0 +1,9 @@
+describe("Testing Jasmine", function() {
+
+  describe("running jasmine", function() {
+    it("should find a spec", function() {
+      console.log("and it did!")
+    })
+  })
+
+})
