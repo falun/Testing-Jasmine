763e7cd7cd03daf3b0c17a11f59838227e501114
daaimah123 <41805952+daaimah123@users.noreply.github.com>
2019-02-09 11:59:03 -0800

    Add Config File & Test Jasmine
    Tried to run jasmine with path and with var name, unsuccessful

--- code_with_dena.js
+++ code_with_dena.js
@@ -4,4 +4,4 @@ var countWords = function(sentence) {
   return sentence.split(" ").length;
 };
 
-module.exports = /Users/codetl/Desktop/Test_Jasmine/code_with_dena.js;
+module.exports = countWords;
